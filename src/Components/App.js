ff7a7fdf671f8b86a55302476d42d1af73a58063
Davanjit Sandhu <davanjit@hotmail.com>
2022-09-28 05:44:42 -0400

    Update App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,7 +2,7 @@ import { HTML5Backend } from 'react-dnd-html5-backend'
 import { TouchBackend } from 'react-dnd-touch-backend'
 
 import { DndProvider } from 'react-dnd'
-import Items from './Test'
+import Items from './Items'
 
 const isTouchDevice = () => {  
   return (('ontouchstart' in window) ||  
